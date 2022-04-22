fa5b659563201e524ec0670ea1ccc0c366c0b0e2
my-first-repo <saiyadul77@gmail.com>
2022-04-22 12:27:24 +0600

    first commit

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,17 +1,19 @@
-import React from 'react';
 import './Cart.css'
 
-const Cart = ({ cart }) => {
+const Cart = ({ cart, chooseToCart }) => {
 
+    // cart = { cart }
+    console.log(cart)
     return (
         <div className='cart'>
             <h2>Selected Furnitures</h2>
-            {cart.map((item) => (
-                <h3 key={item.id}>{item.name}</h3>
-            ))
+            {
+                cart.map((item) => (
+                    <h3 key={item.id}>{item.name}</h3>
+                ))
             }
 
-            <button>
+            <button onClick={() => chooseToCart(cart)}>
                 <p>CHOOSE 1 FOR ME</p>
             </button>
             <br /><br />
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -20,6 +20,7 @@ const Shop = () => {
 
     }
 
+
     return (
         <div className='shop-container'>
             <div className='product-container'>
